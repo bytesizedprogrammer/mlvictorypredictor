2cdd987e6f1ba3556a659d7d3e9eda59eb124a88
Efstathios Plakas <160966005+bytesizedprogrammer@users.noreply.github.com>
2025-01-20 16:57:29 -0500

    Add files via upload

---
+++ SLIPPI JS/slippi-js/src/index.ts
@@ -0,0 +1,20 @@
+// Export melee util types
+export * from "./melee";
+
+// Export types
+export * from "./stats";
+export * from "./types";
+
+// Utils
+export * from "./utils/gameTimer";
+export * from "./utils/slpFile";
+export * from "./utils/slpFileWriter";
+export * from "./utils/slpParser";
+export * from "./utils/slpReader";
+export * from "./utils/slpStream";
+
+// Console networking
+export * from "./console";
+
+// Export the main SlippiGame logic
+export * from "./SlippiGame";
